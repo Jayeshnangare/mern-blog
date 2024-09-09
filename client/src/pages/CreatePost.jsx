4e4c780deeb243c283a2136a602259e2a8150589
Jayeshnangare <jayeshnangare777@gmail.com>
2024-09-09 19:14:11 +0530

    complete create post functionality

--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -6,12 +6,15 @@ import {getDownloadURL,getStorage, ref, uploadBytesResumable} from 'firebase/sto
 import { app } from '../firebase';
 import { CircularProgressbar } from 'react-circular-progressbar';
 import 'react-circular-progressbar/dist/styles.css';
+import {useNavigate} from 'react-router-dom'
 
 export default function CreatePost() {
     const [file,setFile] = useState(null);
     const [imageFileUploadProgress, setImageFileUploadProgress]  = useState(null);
     const [imageFileUploadError, setImageFileUploadError] = useState(null);
-    const [formData, setFormData] = useState({})
+    const [formData, setFormData] = useState({});
+    const [publishError, setPublishError] = useState(null);
+    const navigate = useNavigate();
 
     const handleUploadImage = async () =>{
         try{
@@ -29,7 +32,6 @@ export default function CreatePost() {
                 (snapshot) =>{
                     const progress =
                     (snapshot.bytesTransferred / snapshot.totalBytes)*100   // snapshot gives progrss of upload image
-                    console.log(progress)
                     setImageFileUploadProgress(progress.toFixed(0));
                 },
                 (error) =>{
@@ -53,14 +55,48 @@ export default function CreatePost() {
         }
     }
 
+    const handleSubmit = async (e) =>{
+        e.preventDefault();
+        try{
+            const res = await fetch('/api/post/create',{
+                method: 'POST',
+                headers:{
+                    'Content-Type' : 'application/json',
+                },
+                body: JSON.stringify(formData), 
+            });
+            const data = await res.json();
+
+            if(!res.ok){
+                setPublishError(data.message);
+                return;
+            }
+            if(res.ok){
+                setPublishError(null);
+                navigate(`/post/${data.slug}`)
+            }
+            
+
+        }catch(e){
+            setPublishError('Something Went Wrong');
+        }
+    }
+
   return (
     <div className='p-3 max-w-3xl mx-auto min-h-screen'>
         <h1 className='text-center text-3xl my-7 font-semibold'>Create a post</h1>
-        <form action="" className='flex flex-col gap-4'>
+        <form action="" className='flex flex-col gap-4' onSubmit={handleSubmit}>
             <div className='flex flex-col gap-4 sm:flex-row justify-between'>
-                <TextInput placeholder='Title' id='title' type='text' required
-                className='flex-1'/>
-                <Select>
+                <TextInput 
+                placeholder='Title' 
+                id='title' type='text' 
+                required
+                className='flex-1'
+                onChange={(e) => setFormData({...formData, title: e.target.value})}
+                />
+                <Select 
+                onChange={(e) => setFormData({...formData, category: e.target.value})}
+                >
                     <option value="uncategorized">Select a category</option>
                     <option value="Webdevelopment">Web Development</option>
                     <option value="appdevelopment">App Development</option>
@@ -100,8 +136,19 @@ export default function CreatePost() {
           />
         )
         }
-            <ReactQuill placeholder='Write something...' className='h-72 mb-12'></ReactQuill>
+            <ReactQuill placeholder='Write something...' className='h-72 mb-12'
+            onChange={
+                (value) =>{
+                    setFormData({...formData, content: value})
+                }
+            }
+            ></ReactQuill>
             <Button type='submit' gradientDuoTone='purpleToPink'>Publish</Button>
+            {
+                publishError && <Alert className='mt-5' color={'failure'}>
+                    {publishError}
+                </Alert>
+            }
         </form>
     </div>
   )
